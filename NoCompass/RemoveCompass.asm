ae611c1b8775c8166c6ea8c0a6f1736a84ef43ce
artem <artem.matyushkov@gmail.com>
2025-11-23 00:55:06 +0300

    New approach to remove compass

--- NoCompass/RemoveCompass.asm
+++ NoCompass/RemoveCompass.asm
@@ -1,26 +1,37 @@
 .data
 extern returnAddress : qword
-extern refreshRate   : qword
-extern base          : qword
 
-MIN_VALID          dq 10000h
-MAX_VALID          dq 07FFFFFFFFFFFh
-
-removeCompassCalls dq 0
-compassPtrCached   dq 0
+f_minus_10292       REAL4 -10292.0
+f_minus_9885        REAL4 -9885.0
+f_minus_8293_208984 REAL4 -8293.208984
+f_minus_8763        REAL4 -8763.0
+f_minus_8306        REAL4 -8306.0
 
 .code
-    CheckPtr MACRO reg
-        cmp       reg, MIN_VALID
-        jb        _orig
-        cmp       reg, MAX_VALID
-        ja        _orig
+    IsFEqualToAny2 MACRO reg, offset, val1, val2, label_match
+        movss   xmm0, dword ptr [reg + offset]
+        movss   xmm1, dword ptr [val1]
+        ucomiss xmm0, xmm1
+        je      label_match
+        movss   xmm1, dword ptr [val2]
+        ucomiss xmm0, xmm1
     ENDM
 
-    AddOffset MACRO reg, offset
-        mov       reg, qword ptr [reg + offset]
-        CheckPtr  reg
+    IsFEqualToAny3 MACRO reg, offset, val1, val2, val3, label_match
+        movss   xmm0, dword ptr [reg + offset]
+
+        movss   xmm1, dword ptr [val1]
+        ucomiss xmm0, xmm1
+        je      label_match
+
+        movss   xmm1, dword ptr [val2]
+        ucomiss xmm0, xmm1
+        je      label_match
+
+        movss   xmm1, dword ptr [val3]
+        ucomiss xmm0, xmm1
     ENDM
+
 
     RemoveCompass proc
         movups    xmmword ptr [rbx+80h], xmm6
@@ -30,53 +41,12 @@ compassPtrCached   dq 0
         ptest     xmm7, xmm7
         jz        _orig
 
-        ; Check if need recalc this call
-        mov       rcx, qword ptr [removeCompassCalls]
-        mov       rdx, qword ptr [refreshRate]
+        IsFEqualToAny2 rbx, 64h, f_minus_10292, f_minus_9885, _check_second_value
+        jne     _orig
 
-        inc       rcx
-        cmp       rcx, rdx
-        jb        _use_cached
-
-        xor       rcx, rcx     ; set removeCompassCalls to 0
-        mov       qword ptr [removeCompassCalls], rcx
-        jmp       _recalc
-
-    _use_cached:
-        mov       qword ptr [removeCompassCalls], rcx
-        mov       rdx, qword ptr [compassPtrCached]
-        CheckPtr  rdx
-        jmp       _compare_ptr
-
-    ; Calculate compass pointer = base + 3B42FB8 + 40 + 30 + 88 + D8 + 110 + D8 + 30 + 48 + 70 + 90
-    _recalc:
-        mov       rax, qword ptr [base]
-        add       rax, 3B42FB8h
-
-        mov       rax, qword ptr [rax]
-        CheckPtr  rax
-
-        AddOffset rax, 40h
-        AddOffset rax, 30h
-        AddOffset rax, 88h
-        AddOffset rax, 0D8h
-        AddOffset rax, 110h
-        AddOffset rax, 0D8h
-        AddOffset rax, 30h
-        AddOffset rax, 48h
-        AddOffset rax, 70h
-
-        lea       rdx, [rax+90h]
-        CheckPtr  rdx
-
-        ; Cache compass pointer
-        mov       qword ptr [compassPtrCached], rdx
-
-    ; If rbx+90 is not a compass pointer => skip
-    _compare_ptr:
-        lea       rcx, [rbx+90h]
-        cmp       rcx, rdx
-        jne       _orig
+    _check_second_value:
+        IsFEqualToAny3 rbx, 6Ch, f_minus_8293_208984, f_minus_8763, f_minus_8306, _write_zero
+        jne     _orig
 
     _write_zero:
         xorps     xmm0, xmm0
